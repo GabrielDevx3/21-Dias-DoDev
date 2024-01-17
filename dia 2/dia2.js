fcb9d02111dbf16ff5846e5538d8ddab186b8a85
Lucas Gabriel <lucas0908gabriel@gmail.com>
2024-01-17 09:38:11 -0300

    teste

--- dia 2/dia2.js
+++ dia 2/dia2.js
@@ -1,1 +1,10 @@
-console.log("Olá,  Mundo!!, Atividade de GIT")
+// let e cont 
+//let é para inserir uma variavel, dando a possibilidade de altera-la 
+//const é para inserir uma variavel, mas não da a possibilidade de altera-la
+
+//===================EXEMPLOS=======================
+
+let meunome = 'Lucas Gabriel'
+console.log("Meu nome é: ", meunome)
+meunome = 'Lucas Silva'
+console.log("Meu primeiro e ultimo nome é: ", meunome)
